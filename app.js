5c9e86200d325ecc7158e14fba412e0194452695
Jkoko1 <jerryspencer91@gmail.com>
2022-05-05 11:12:52 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// Question1
+
+let array1 = [20, 46, 90, 80, 120];
+
+let array2 = [2, 2, 5, 4, 2];
+
+var set1 = array1[0] / array2[0];
+var set2 = array1[1] / array2[1];
+var set3 = array1[2] / array2[2];
+var set4 = array1[3] / array2[3];
+var set5 = array1[4] / array2[4];
+
+let array3 = new Array();
+
+array3.push(set1, set2, set3, set4, set5);
+
+alert(array3);
+
+// Question2
+
+var arr = [[1, 2, 3], [4, 5, 6], 7, 8, 9, [[10, 11, 12, [13, 4, 112]], 136, 14]];
